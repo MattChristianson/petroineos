11b7ecd5de6050373943f66e5d7c2d05a9a3174d
Matthew Christianson <mchris2455@gmail.com>
2022-10-12 18:58:36 +0100

    Add files via upload

---
+++ PPTR.Domain/ReportOptions.cs
@@ -0,0 +1,20 @@
+﻿namespace PPTR.Domain
+{
+    public class ReportOptions
+    {
+        /// <summary>
+        /// Interval in Seconds
+        /// </summary>
+        public double Interval { get; set; }
+        public string? ReportsPath { get; set; }
+        public string? LogFilePath { get; set; }
+        /// <summary>
+        /// Service timepout in seconds
+        /// </summary>
+        public double Timeout { get; set; }
+        /// <summary>
+        /// Number of service retry attempts
+        /// </summary>
+        public int RetryAttempts  { get; set; }
+    }
+}
